9111b117653cb6b02e96612284532ea9831ec176
harrygg95 <carlos.p.g.g95@gmail.com>
2025-10-01 13:43:57 -0300

    refactor: restructure app into src directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import { DataProvider } from './context/DataContext';
 import { NotificationProvider } from './context/NotificationContext';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <DataProvider>
       <NotificationProvider>
